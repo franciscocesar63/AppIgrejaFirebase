097bebe907ed69bf0c23619df788e408a6fcc12e
franciscocesar63 <franciscocesar888@gmail.com>
2021-03-08 15:08:26 -0300

    App criado e realizado conexão com Firebase

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:app_igreja/pages/HomePage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Firestore.instance.collection('estudo').getDocuments().then((value) {
+      value.documents.forEach((element) {
+        print(element.data);
+      });
+    });
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
